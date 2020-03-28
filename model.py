153f866cad8e3df6ee0329a321d7290b190831c9
Eric Roeum <eroeum@gmail.com>
2020-03-28 00:38:33 -0400

    Utilize data generator and batches

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
 
 class Model(object):
     def __init__(self, proto=False, min_delta=0.001):
@@ -11,6 +12,28 @@ class Model(object):
         self.earlystop_callback = tf.keras.callbacks.EarlyStopping(
             monitor='accuracy', min_delta=min_delta,
             patience=1)
+        self.datagen = ImageDataGenerator(
+            featurewise_center=False,
+            samplewise_center=False,
+            featurewise_std_normalization=False,
+            samplewise_std_normalization=False,
+            zca_whitening=False,
+            zca_epsilon=1e-06,
+            rotation_range=0,
+            width_shift_range=0.1,
+            height_shift_range=0.1,
+            shear_range=0.,
+            zoom_range=0.,
+            channel_shift_range=0.,
+            fill_mode='nearest',
+            cval=0.,
+            horizontal_flip=True,
+            vertical_flip=False,
+            rescale=None,
+            preprocessing_function=None,
+            data_format=None,
+            validation_split=0.0)
+
 
     @classmethod
     def build_model(self):
@@ -31,12 +54,14 @@ class Model(object):
         ])
         return model
 
-    def run(self, predictors, targets, epochs=10):
+    def run(self, predictors, targets, epochs=80, batch_size=64):
+        self.datagen.fit(predictors)
         if(self.proto):
-            self.model.fit(predictors, targets,
+            self.model.fit_generator(self.datagen.flow(predictors, targets, batch_size=batch_size),
                 epochs=epochs, callbacks=[self.earlystop_callback])
         else:
-            self.model.fit(predictors, targets, epochs=epochs)
+            self.model.fit_generator(self.datagen.flow(predictors, targets, batch_size=batch_size),
+                epochs=epochs)
 
     def evaluate(self, predictors, targets, verbosity=2):
         loss, acc = self.model.evaluate(predictors, targets,
